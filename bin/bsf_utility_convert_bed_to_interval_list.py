46c759ac81ff09be1a079de228365480b9503680
mkschuster <mschuster@cemm.oeaw.ac.at>
2015-09-15 14:45:51 +0200

    Write annotation lines in the same order in which sequence region names are declared in the sequence dictionary (@SQ header lines), further sorted by start position. Provide some initial support to convert from UCSC Genome Browser to Genome Reference Consortium (GRC) sequence region names.

--- bin/bsf_utility_convert_bed_to_interval_list.py
+++ bin/bsf_utility_convert_bed_to_interval_list.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 #
-# BSF Python script to convert a BED file into a Picard intervals file.
+# BSF Python script to convert a BED file into a Picard interval list file.
 #
 #
 # Copyright 2014 Michael K. Schuster
@@ -38,7 +38,7 @@ os.environ['LANG'] = 'C'
 # Parse the arguments.
 
 argument_parser = argparse.ArgumentParser(
-    description='BSF utility to convert a BED file into a Picard intervals file.')
+    description='BSF utility to convert a BED file into a Picard interval list file.')
 
 argument_parser.add_argument(
     '--debug',
@@ -64,22 +64,37 @@ argument_parser.add_argument(
     required=True)
 
 argument_parser.add_argument(
-    '--remove-chr-prefix',
+    '--ucsc-to-grc',
     action='store_true',
-    dest='no_prefix',
-    help="remove a UCSC-style 'chr' prefix from the sequence name")
+    dest='ucsc_to_grc',
+    help="convert UCSC-style 'chr' prefixed sequence names to GRC reference names")
 
 name_space = argument_parser.parse_args()
 
 output_file = open(name_space.output_path, 'wb')
 
-# Read the SAM header dictionary and copy to the output file.
+# Read the SAM header dictionary and copy it to the output file.
+
 input_file = open(name_space.dictionary, 'rb')
+
+# To retain the order of sequence regions as defined in the sequence dictionary (i.e. SAM header),
+# build a Python list of sequence region names and a Python dict of Python str (sequence region name) key
+# and Python list of Python list (interval) objects.
+
+sequence_name_dict = dict()
+sequence_name_list = list()
 
 for line in input_file:
     output_file.write(line)
+    if line.startswith('@SQ'):
+        for sam_field in line.split('\t'):
+            if sam_field[:3] == 'SN:':
+                sequence_name_dict[sam_field[3:]] = list()
+                sequence_name_list.append(sam_field[3:])
 
 input_file.close()
+
+# Read the BED file.
 
 input_file = open(name_space.input_path, 'rb')
 
@@ -94,8 +109,13 @@ for line in input_file:
 
     interval_fields = list()
 
-    if name_space.no_prefix and bed_fields[0][:3] == 'chr':  # Sequence with or without 'chr'.
-        interval_fields.append(bed_fields[0][3:])
+    if name_space.ucsc_to_grc:
+        if bed_fields[0] == 'chrM':  # The 'chrM' needs converting into 'MT'.
+            interval_fields.append('MT')
+        elif bed_fields[0][:3] == 'chr':  # Sequence with or without 'chr'.
+            interval_fields.append(bed_fields[0][3:])
+        else:
+            interval_fields.append(bed_fields[0])
     else:
         interval_fields.append(bed_fields[0])
 
@@ -104,8 +124,21 @@ for line in input_file:
     interval_fields.append('+')  # Strand
     interval_fields.append(bed_fields[3])  # Name
 
-    output_file.write(string.join(words=interval_fields, sep="\t") + "\n")
+    sequence_name_dict[interval_fields[0]].append(interval_fields)
 
 input_file.close()
+
+# Write interval lines in the order of sequence region names in the sequence dictionary (i.e. SAM header) file.
+
+for sequence_name in sequence_name_list:
+    assert isinstance(sequence_name, str)
+    interval_list = sequence_name_dict[sequence_name]
+    assert isinstance(interval_list, list)
+    print "Sequence name: {} lines: {}".format(sequence_name, len(interval_list))
+    # Sort numerically on the sequence region start field.
+    interval_list.sort(cmp=lambda x, y: cmp(int(x[1]), int(y[1])))
+
+    for interval_fields in interval_list:
+        output_file.write(string.join(words=interval_fields, sep="\t") + "\n")
 
 output_file.close()
